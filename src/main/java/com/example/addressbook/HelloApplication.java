32cf3170405092b8cce0a0a4ee71f6b2f439829b
Teresa Paatero <lotta.paatero@aalto.fi>
2025-03-25 11:19:32 +1000

    Excercise completed

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -8,14 +8,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static final String TITLE = "Address Book";
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 360;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
