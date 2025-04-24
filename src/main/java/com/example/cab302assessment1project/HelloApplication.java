e06a6d9c32d75afe6e6df25176de8c67f49f28a3
FindabhairDoolan <n11957557@qut.edu.au>
2025-04-25 00:19:44 +1000

    removed accidental comment

--- src/main/java/com/example/cab302assessment1project/HelloApplication.java
+++ src/main/java/com/example/cab302assessment1project/HelloApplication.java
@@ -7,9 +7,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * A class
- */
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
