bb1d260514f47044ce4eba34a155b41a98997b00
FindabhairDoolan <n11957557@qut.edu.au>
2025-04-28 04:05:51 +1000

    Added back button functionality

--- src/main/java/com/example/cab302assessment1project/Controller/CreateQuizController.java
+++ src/main/java/com/example/cab302assessment1project/Controller/CreateQuizController.java
@@ -1,12 +1,28 @@
 package com.example.cab302assessment1project.Controller;
 
+import com.example.cab302assessment1project.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateQuizController {
+    public Button backButton;
 
     public void onCreate(ActionEvent actionEvent) {
     }
 
-    public void onBack(ActionEvent actionEvent) {
+    //When back button is pressed, returns user to previous page
+    @FXML
+    public void onBack(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+        //This will lead back to the home page in future
     }
 }
