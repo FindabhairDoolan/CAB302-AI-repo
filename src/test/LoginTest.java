c47c04420a9e095b1d147614cf2cbe2cba53ce2c
Teresa Paatero <lotta.paatero@aalto.fi>
2025-05-01 17:20:05 +1000

    Added files for tests

---
+++ src/test/LoginTest.java
@@ -0,0 +1,9 @@
+import com.example.quizapp.Models.Quiz;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+
+}
